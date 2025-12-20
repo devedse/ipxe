e107cb117b52c38e06dc8b361e4ecd646e3a1aeb
Devedse <davydavidse@gmail.com>
2025-12-20 23:25:02 +0100

    [feature] Add support for storing PNP list output to a variable

--- src/hci/commands/pnplist_cmd.c
+++ src/hci/commands/pnplist_cmd.c
@@ -31,6 +31,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 #include <ipxe/command.h>
 #include <ipxe/parseopt.h>
 #include <ipxe/malloc.h>
+#include <ipxe/settings.h>
 #ifdef PLATFORM_efi
 #include <ipxe/efi/efi.h>
 #include <ipxe/efi/efi_pci.h>
@@ -45,14 +46,21 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  */
 
 /** "pnplist" options */
-struct pnplist_options {};
+struct pnplist_options {
+	/** Variable name to store output */
+	char *store;
+};
 
 /** "pnplist" option list */
-static struct option_descriptor pnplist_opts[] = {};
+static struct option_descriptor pnplist_opts[] = {
+	OPTION_DESC ( "store", 's', required_argument,
+		      struct pnplist_options, store, parse_string ),
+};
 
 /** "pnplist" command descriptor */
 static struct command_descriptor pnplist_cmd =
-	COMMAND_DESC ( struct pnplist_options, pnplist_opts, 0, 0, "" );
+	COMMAND_DESC ( struct pnplist_options, pnplist_opts, 0, 0,
+		       "[--store <variable>]" );
 
 /**
  * Detect if device info appears to be abstracted by SNP or virtualization
@@ -85,7 +93,9 @@ static struct pci_device * try_efi_pci_access ( struct net_device *netdev, int *
 	int rc;
 
 	/* Find SNP device for this network device */
+	printf ( "DEBUG: netdev=%p name='%s' state=0x%x link_rc=%d\n", netdev, netdev->name, netdev->state, netdev->link_rc );
 	snpdev = find_snpdev_by_netdev ( netdev );
+	printf ( "DEBUG: snpdev=%p handle=%p parent=%p started=%d\n", snpdev, snpdev ? snpdev->handle : NULL, snpdev ? snpdev->parent : NULL, snpdev ? snpdev->started : 0 );
 	if ( snpdev ) {
 		
 		/* Get the EFI device handle - this should be the PCI device */
@@ -98,7 +108,9 @@ static struct pci_device * try_efi_pci_access ( struct net_device *netdev, int *
 		}
 
 		rc = efipci_info ( device_handle, efipci );
+		printf ( "DEBUG: efipci_info returned rc=%d\n", rc );
 		if ( rc == 0 ) {
+			printf ( "DEBUG: Got real PCI device via EFI PCI I/O protocol - vendor=0x%04x device=0x%04x\n", efipci->pci.vendor, efipci->pci.device );
 			
 			/* Allocate a regular PCI device structure and copy the information */
 			pci = malloc ( sizeof ( *pci ) );
@@ -109,8 +121,10 @@ static struct pci_device * try_efi_pci_access ( struct net_device *netdev, int *
 				return pci;
 			}
 		} else {
+			printf ( "DEBUG: Failed to get PCI device via EFI PCI I/O protocol\n" );
 			free ( efipci );
 	} else {
+		printf ( "DEBUG: SNP device not found for netdev\n" );
 		*need_free = 0;
 #else
 	/* Suppress unused parameter warning on non-EFI platforms */
@@ -139,20 +153,27 @@ static struct pci_device * get_real_pci_device ( struct net_device *netdev, int 
 	*need_free = 0;
 	
 	/* First, try EFI PCI I/O protocol access to bypass SNP abstraction */
+	printf ( "DEBUG: Attempting EFI PCI I/O access for netdev '%s'\n", netdev->name );
 	pci = try_efi_pci_access ( netdev, need_free );
 	if ( pci ) {
+		printf ( "DEBUG: Got real PCI device via EFI - vendor=0x%04x device=0x%04x bus=%d slot=%d func=%d\n", pci->vendor, pci->device, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ), PCI_FUNC ( pci->busdevfn ) );
 		return pci;
 	}
 	
 	/* Fall back to direct PCI device access */
+	printf ( "DEBUG: Falling back to direct PCI device access\n" );
 	if ( dev->desc.bus_type == BUS_TYPE_PCI ) {
 		pci = container_of ( dev, struct pci_device, dev );
+		printf ( "DEBUG: Direct PCI device found - vendor=0x%04x device=0x%04x bus=%d slot=%d func=%d\n", pci->vendor, pci->device, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ), PCI_FUNC ( pci->busdevfn ) );
 		
 		/* Check if we have abstracted device information */
 		if ( is_snp_abstracted ( pci->vendor, pci->device ) ) {
+			printf ( "DEBUG: WARNING - Device appears to have SNP abstracted IDs (0x%04x:0x%04x)\n", pci->vendor, pci->device );
 		}
 		
 		return pci;
+	} else {
+		printf ( "DEBUG: Device bus_type is not PCI (bus_type=%d)\n", dev->desc.bus_type );
 	}
 
 	return NULL;
@@ -162,53 +183,88 @@ static struct pci_device * get_real_pci_device ( struct net_device *netdev, int 
  * Display Windows-style PNP device path for a network device
  *
  * @v netdev		Network device
+ * @v buffer		Output buffer (or NULL to print to console)
+ * @v len		Buffer length
+ * @ret used		Number of bytes used in buffer
  * @ret rc		Return status code
  */
-static int pnplist_show_device ( struct net_device *netdev ) {
+static int pnplist_show_device ( struct net_device *netdev, char *buffer, size_t len, size_t *used ) {
 	struct pci_device *pci;
 	uint16_t subsys_vendor = 0x0000, subsys_device = 0x0000;
 	uint8_t revision = 0x00;
 	int rc;
 	int need_free = 0;
+	size_t pos = 0;
+	int n;
+
+	printf ( "\nDEBUG: ==== Processing device '%s' ====\n", netdev->name );
 
 	/* Get the real PCI device information */
 	pci = get_real_pci_device ( netdev, &need_free );
 	if ( ! pci ) {
+		printf ( "DEBUG: Could not identify PCI device for '%s', skipping\n" , netdev->name );
 		return 0; /* Skip devices we can't identify */
 	}
+	printf ( "DEBUG: Successfully obtained PCI device (need_free=%d)\n", need_free );
 
 	/* Try to read subsystem vendor ID */
 	rc = pci_read_config_word ( pci, PCI_SUBSYSTEM_VENDOR_ID, &subsys_vendor );
+	printf ( "DEBUG: Read subsystem vendor ID: rc=%d value=0x%04x\n", rc, subsys_vendor );
 	if ( rc != 0 ) {
 		subsys_vendor = 0x0000;
 	}
 
 	/* Try to read subsystem device ID */
 	rc = pci_read_config_word ( pci, PCI_SUBSYSTEM_ID, &subsys_device );
+	printf ( "DEBUG: Read subsystem device ID: rc=%d value=0x%04x\n", rc, subsys_device );
 	if ( rc != 0 ) {
 		subsys_device = 0x0000;
 	}
 
 	/* Try to read revision */
 	rc = pci_read_config_byte ( pci, PCI_REVISION, &revision );
+	printf ( "DEBUG: Read revision: rc=%d value=0x%02x\n", rc, revision );
 	if ( rc != 0 ) {
 		revision = 0x00;
 	}
 
 	/* Display PNP device path in Windows format */
+	printf ( "DEBUG: Final values - vendor=0x%04x device=0x%04x subsys_vendor=0x%04x subsys_device=0x%04x rev=0x%02x\n", pci->vendor, pci->device, subsys_vendor, subsys_device, revision );
 	if ( ( subsys_vendor == 0x0000 ) || ( subsys_device == 0x0000 ) ||
 	     ( subsys_vendor == 0xFFFF ) || ( subsys_device == 0xFFFF ) ) {
+		printf ( "DEBUG: Using fallback subsystem IDs (invalid subsystem)\n" );
 		/* No valid subsystem ID - use vendor and device ID as fallback */
-		printf ( "PCI\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X\\%X&%X&%X&%X\n",
-			 pci->vendor, pci->device, pci->device, pci->vendor,
-			 revision, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ),
-			 PCI_FUNC ( pci->busdevfn ), pci->busdevfn );
+		if ( buffer ) {
+			n = snprintf ( buffer + pos, ( pos < len ) ? ( len - pos ) : 0,
+				       "PCI\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X\\%X&%X&%X&%X\n",
+				       pci->vendor, pci->device, pci->device, pci->vendor,
+				       revision, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ),
+				       PCI_FUNC ( pci->busdevfn ), pci->busdevfn );
+			if ( n > 0 )
+				pos += n;
+		} else {
+			printf ( "PCI\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X\\%X&%X&%X&%X\n",
+				 pci->vendor, pci->device, pci->device, pci->vendor,
+				 revision, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ),
+				 PCI_FUNC ( pci->busdevfn ), pci->busdevfn );
+		}
 	} else {
+		printf ( "DEBUG: Using actual subsystem IDs\n" );
 		/* Use actual subsystem IDs */
-		printf ( "PCI\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X\\%X&%X&%X&%X\n",
-			 pci->vendor, pci->device, subsys_device, subsys_vendor,
-			 revision, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ),
-			 PCI_FUNC ( pci->busdevfn ), pci->busdevfn );
+		if ( buffer ) {
+			n = snprintf ( buffer + pos, ( pos < len ) ? ( len - pos ) : 0,
+				       "PCI\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X\\%X&%X&%X&%X\n",
+				       pci->vendor, pci->device, subsys_device, subsys_vendor,
+				       revision, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ),
+				       PCI_FUNC ( pci->busdevfn ), pci->busdevfn );
+			if ( n > 0 )
+				pos += n;
+		} else {
+			printf ( "PCI\\VEN_%04X&DEV_%04X&SUBSYS_%04X%04X&REV_%02X\\%X&%X&%X&%X\n",
+				 pci->vendor, pci->device, subsys_device, subsys_vendor,
+				 revision, PCI_BUS ( pci->busdevfn ), PCI_SLOT ( pci->busdevfn ),
+				 PCI_FUNC ( pci->busdevfn ), pci->busdevfn );
+		}
 	}
 
 	/* Free allocated PCI device structure if needed */
@@ -216,6 +272,7 @@ static int pnplist_show_device ( struct net_device *netdev ) {
 		free ( pci );
 	}
 
+	*used = pos;
 	return 0;
 }
 
@@ -229,16 +286,62 @@ static int pnplist_show_device ( struct net_device *netdev ) {
 static int pnplist_exec ( int argc, char **argv ) {
 	struct pnplist_options opts;
 	struct net_device *netdev;
+	char *output = NULL;
+	size_t output_len = 0;
+	size_t total_used = 0;
+	size_t used;
 	int rc;
 
 	/* Parse options */
 	if ( ( rc = parse_options ( argc, argv, &pnplist_cmd, &opts ) ) != 0 )
 		return rc;
 
+	/* If storing to a variable, allocate output buffer */
+	if ( opts.store ) {
+		/* Initial buffer size - will grow if needed */
+		output_len = 1024;
+		output = malloc ( output_len );
+		if ( ! output )
+			return -ENOMEM;
+		output[0] = '\0';
+	}
+
 	/* Iterate through all network devices */
 	for_each_netdev ( netdev ) {
-		if ( ( rc = pnplist_show_device ( netdev ) ) != 0 )
-			return rc;
+		if ( opts.store ) {
+			/* Check if we need more buffer space */
+			while ( total_used + 256 >= output_len ) {
+				char *new_output;
+				output_len *= 2;
+				new_output = realloc ( output, output_len );
+				if ( ! new_output ) {
+					free ( output );
+					return -ENOMEM;
+				}
+				output = new_output;
+			}
+			if ( ( rc = pnplist_show_device ( netdev, output + total_used,
+							  output_len - total_used, &used ) ) != 0 ) {
+				free ( output );
+				return rc;
+			}
+			total_used += used;
+		} else {
+			if ( ( rc = pnplist_show_device ( netdev, NULL, 0, &used ) ) != 0 )
+				return rc;
+		}
+	}
+
+	/* Store output to variable if requested */
+	if ( opts.store ) {
+		printf ( "DEBUG: Storing %zu bytes to variable '%s'\n", total_used, opts.store );
+		rc = store_setting ( NULL, opts.store, output, total_used );
+		if ( rc != 0 ) {
+			printf ( "Could not store to variable \"%s\": %s\n",
+				 opts.store, strerror ( rc ) );
+		}
+		free ( output );
+		return rc;
 	}
 
 	return 0;
