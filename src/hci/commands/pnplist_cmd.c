094fd41598825f3883337879072e4c2f452d1ee0
Devedse <davydavidse@gmail.com>
2025-12-21 01:53:28 +0100

    [fix] Adjust debug output format for variable storage in pnplist_exec function

--- src/hci/commands/pnplist_cmd.c
+++ src/hci/commands/pnplist_cmd.c
@@ -339,7 +339,7 @@ static int pnplist_exec ( int argc, char **argv ) {
 	/* Store output to variable if requested */
 	if ( opts.store ) {
 		struct named_setting setting;
-		printf ( "DEBUG: Storing %d bytes to variable '%s'\n", ( int ) total_used, opts.store );
+		printf ( "\nDEBUG: Storing %d bytes to variable '%s'\n", ( int ) total_used, opts.store );
 		/* Parse setting name */
 		if ( ( rc = parse_autovivified_setting ( opts.store, &setting ) ) != 0 ) {
 			printf ( "Could not parse setting name \"%s\": %s\n",
